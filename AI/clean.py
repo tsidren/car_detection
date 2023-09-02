c16190698187640df768a5e96c15589f9742a815
tsidren <shivamsinghhiro@gmail.com>
2023-09-02 16:33:00 +0530

    one variable change

--- AI/clean.py
+++ AI/clean.py
@@ -6,8 +6,10 @@ csv_file = '../data/vehicle_details.csv'
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv(csv_file)
 
+licence_plate_digits = 6
+
 # Filter the DataFrame based on the condition
-filtered_df = df[(df.iloc[:, 1].str.len() >= 6) & (~df.iloc[:, 1].duplicated())]
+filtered_df = df[(df.iloc[:, 1].str.len() >= licence_plate_digits) & (~df.iloc[:, 1].duplicated())]
 
 # Save the filtered DataFrame to a new CSV file
 output_file = '../data/filtered_file.csv'
